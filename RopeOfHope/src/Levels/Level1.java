d181de823de1140352a2911b13b06f1fb084ae6e
103816 <103816@10.113.82.171>
2016-10-11 12:07:10 -0400

    g

--- RopeOfHope/src/Levels/Level1.java
+++ RopeOfHope/src/Levels/Level1.java
@@ -32,7 +32,7 @@ public class Level1 extends JFrame {
 	 */
 	public Level1() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 700, 450);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
