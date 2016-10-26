c9426616c08e3899f5134c0660bfbaa964c0f7c3
103816 <103816@10.113.82.217>
2016-10-26 12:51:32 -0400

    t

--- RopeOfHope/src/game/Physics.java
+++ RopeOfHope/src/game/Physics.java
@@ -6,7 +6,10 @@ public class Physics {
 	public Physics(){
 		
 	}
-	
+	//even if you use ints it still won't work well because you can go deep enough into thin walls within one frame, 
+	//the player could get stuck in the middle of a wall with the collisions all saying "not touching anything" because the
+	//edges aren't but the center is
+	//much better to treat it as one object
 	public Boolean isAbleMoveRight(int x1, int y1, int width1, int height1, int x2, int y2, int width2, int height2){
 		
 		return true;
