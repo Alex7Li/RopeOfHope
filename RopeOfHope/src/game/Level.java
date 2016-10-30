c39ba5cc7d353be7c291bbd5fbca5aa1987baf53
103816 <103816@192.168.0.14>
2016-10-29 20:58:42 -0400

    Added comments, hit command-shift-F

--- RopeOfHope/src/game/Level.java
+++ RopeOfHope/src/game/Level.java
@@ -11,88 +11,82 @@ import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-
 @SuppressWarnings("serial")
 public class Level extends JPanel implements ActionListener, KeyListener {
-	
+
 	private Timer t = new Timer(30, this);
 	Rope rope = new Rope();
-    Physics physics = new Physics();
-    static int level = 1;   
+	Physics physics = new Physics();
+	static int level = 1;
 	Player circle = new Player(MainMenu.getCirclex(level), MainMenu.getCircley(level));
 	Player square = new Player(MainMenu.getSquarex(level), MainMenu.getSquarey(level));
 	Exit exit = new Exit(MainMenu.getExitx(level), MainMenu.getExity(level));
-	static int[] wallNums = {6,6};
-	//wallNums = number of walls in level corresponding to array position.
-	
-	static int[][] xarrs = {
-		{ 0,   850, 0, 150, 40, 253},
-		{ 0,   650, 0,   150, 40, 253}
-	};
-	//each 1-D array level is a level's x-block positions.
-	static int[][] yarrs = {     
-		{ 0,   0,   550, 280, 150, 500},
-		{ 0,   0,   550, 280, 150, 500}
-		
-	};
-	
-	static int[][] widtharrs = {
-		{ 40,  50,  700, 100, 50 , 32},
-		{ 40,  50,  700, 100, 50 , 32}
-	};
-	
-	static int[][] heightarrs = {
-		{ 700, 700, 40, 100, 30 , 10},
-		{ 700, 700, 40, 340, 30 , 330}
+	static int[] wallNums = { 6, 6 };
+	// wallNums = number of walls in level corresponding to array position.
+
+	static int[][] xarrs = { { 0, 850, 0, 150, 40, 253 }, { 0, 650, 0, 150, 40, 253 } };
+	// each 1-D array level is a level's x-block positions.
+	static int[][] yarrs = { { 0, 0, 550, 280, 150, 500 }, { 0, 0, 550, 280, 150, 500 }
 
 	};
 
-	public static Rectangle[]  walls = new Rectangle[wallNums[level-1]];
-	
+	static int[][] widtharrs = { { 40, 50, 700, 100, 50, 32 }, { 40, 50, 700, 100, 50, 32 } };
+
+	static int[][] heightarrs = { { 700, 700, 40, 100, 30, 10 }, { 700, 700, 40, 340, 30, 330 }
+
+	};
+
+	public static Rectangle[] walls = new Rectangle[wallNums[level - 1]];
+
 	public Level() {
 		setFocusable(true);
 		addKeyListener(this);
-		setBackground(Color.WHITE);	
-	    level = MainMenu.getLevel();    
-		int[] x = new int[wallNums[level-1]];
-		for (int i=0; i < x.length; i++) {
-	           x[i]=xarrs[level-1][i];
+		setBackground(Color.WHITE);
+		level = MainMenu.getLevel();
+		// creates arrays that hold the rectangles from the main class array.
+		// I couldn't find a better way to do this because of the way arrays are
+		// declared.
+		int[] x = new int[wallNums[level - 1]];
+		for (int i = 0; i < x.length; i++) {
+			x[i] = xarrs[level - 1][i];
 		}
-		int[] y = new int[wallNums[level-1]];   
-		for (int i=0; i<y.length; i++) {
-	           y[i]=yarrs[level-1][i];
+		int[] y = new int[wallNums[level - 1]];
+		for (int i = 0; i < y.length; i++) {
+			y[i] = yarrs[level - 1][i];
 		}
-		int[] width =  new int[wallNums[level-1]];   
-		for (int i=0; i<width.length; i++) {
-	           width[i]=widtharrs[level-1][i];
+		int[] width = new int[wallNums[level - 1]];
+		for (int i = 0; i < width.length; i++) {
+			width[i] = widtharrs[level - 1][i];
 		}
 
-		int[] height = new int[wallNums[level-1]];   
-		for (int i=0; i<height.length; i++) {
-	           height[i]=heightarrs[level-1][i];
+		int[] height = new int[wallNums[level - 1]];
+		for (int i = 0; i < height.length; i++) {
+			height[i] = heightarrs[level - 1][i];
 		}
 		for (int i = 0; i < x.length; i++) {
 			Rectangle r = new Rectangle(x[i], y[i], width[i], height[i]);
 			walls[i] = r;
-		}		
+		}
 		setLayout(null);
 		t.start();
 		setVisible(true);
 		repaint();
 	}
-	
-	public void nextLevel(){
+
+	public void nextLevel() {
+		// closes this level and starts the next one
 		setVisible(false);
 		MainMenu.incrementLevel();
 		MainMenu.startGame();
-		
 	}
+
 	public void paint(Graphics g) {
+		// paints the initial elements
 		super.paint(g);
 		g.setColor(Color.BLACK);
-		
-		for (int i = 0; i < xarrs[level-1].length; i++) {
-			g.fillRect(xarrs[level-1][i], yarrs[level-1][i], widtharrs[level-1][i], heightarrs[level-1][i]);
+
+		for (int i = 0; i < xarrs[level - 1].length; i++) {
+			g.fillRect(xarrs[level - 1][i], yarrs[level - 1][i], widtharrs[level - 1][i], heightarrs[level - 1][i]);
 		}
 		g.setColor(Color.GREEN);
 
@@ -100,19 +94,21 @@ public class Level extends JPanel implements ActionListener, KeyListener {
 	}
 
 	public void paintComponent(Graphics g) {
+		// elements that are repainted each frame
 		super.paintComponent(g);
+		// characters
 		g.setColor(Color.BLUE);
 		g.fillRect(square.getx(), square.gety(), square.getSize(), square.getSize());
 		g.setColor(Color.RED);
 		g.fillOval(circle.getx(), circle.gety(), circle.getSize(), circle.getSize());
 		g.setColor(Color.BLACK);
-		
+		// rope
 		g.drawLine(circle.getx() + square.getSize() / 2, circle.gety() + square.getSize() / 2,
-		square.getx() + square.getSize() / 2, square.gety() + square.getSize() / 2);
-		
-		double length = Math.hypot(circle.getx()-square.getx(), circle.gety()-square.gety());
-		double angle = Math.atan2(circle.gety()-square.gety(), circle.getx()-square.getx());
-		
+				square.getx() + square.getSize() / 2, square.gety() + square.getSize() / 2);
+		// just variables for the rope class
+		double length = Math.hypot(circle.getx() - square.getx(), circle.gety() - square.gety());
+		double angle = Math.atan2(circle.gety() - square.gety(), circle.getx() - square.getx());
+
 		rope.setLength(length);
 		rope.setAngle(angle);
 	}
@@ -120,31 +116,42 @@ public class Level extends JPanel implements ActionListener, KeyListener {
 	public void keyPressed(KeyEvent e) {
 		int k = e.getKeyCode();
 
-		switch(k){
+		switch (k) {
 		case KeyEvent.VK_RIGHT:
-			circle.setRightKeyPressed(true); break;
+			circle.setRightKeyPressed(true);
+			break;
 		case KeyEvent.VK_LEFT:
-			circle.setLeftKeyPressed(true); break;
+			circle.setLeftKeyPressed(true);
+			break;
 		case KeyEvent.VK_UP:
-			circle.jump(); break;
+			circle.jump();
+			break;
 		case KeyEvent.VK_DOWN:
-			circle.setDownKeyPressed(true); break;
+			circle.setDownKeyPressed(true);
+			break;
 		case KeyEvent.VK_0:
-			nextLevel(); break;
+			nextLevel();
+			break;
 		case KeyEvent.VK_M:
-	        this.setVisible(false);
-	        MainMenu.setLevel(1);
-	        new MainMenu().setVisible(true); break;
+			this.setVisible(false);
+			MainMenu.setLevel(1);
+			new MainMenu().setVisible(true);
+			break;
 		}
-		switch(k){
+
+		switch (k) {
 		case KeyEvent.VK_D:
-			square.setRightKeyPressed(true); break;
+			square.setRightKeyPressed(true);
+			break;
 		case KeyEvent.VK_A:
-			square.setLeftKeyPressed(true); break;
+			square.setLeftKeyPressed(true);
+			break;
 		case KeyEvent.VK_W:
-			square.jump(); break;
+			square.jump();
+			break;
 		case KeyEvent.VK_S:
-			square.setDownKeyPressed(true); break;
+			square.setDownKeyPressed(true);
+			break;
 		}
 	}
 
@@ -155,30 +162,36 @@ public class Level extends JPanel implements ActionListener, KeyListener {
 	public void keyReleased(KeyEvent e) {
 		int k = e.getKeyCode();
 
-		switch(k){
+		switch (k) {
 		case KeyEvent.VK_RIGHT:
-			circle.setRightKeyPressed(false); break;
+			circle.setRightKeyPressed(false);
+			break;
 		case KeyEvent.VK_LEFT:
-			circle.setLeftKeyPressed(false); break;
+			circle.setLeftKeyPressed(false);
+			break;
 		case KeyEvent.VK_UP:
-			circle.jump(); break;
+			circle.jump();
+			break;
 		case KeyEvent.VK_DOWN:
-			circle.setDownKeyPressed(false); break;
+			circle.setDownKeyPressed(false);
+			break;
 		}
-		switch(k){
-			case KeyEvent.VK_D:
-				square.setRightKeyPressed(false); break;
-			case KeyEvent.VK_A:
-				square.setLeftKeyPressed(false); break;
-			case KeyEvent.VK_S:
-				square.setDownKeyPressed(false); break;
+		switch (k) {
+		case KeyEvent.VK_D:
+			square.setRightKeyPressed(false);
+			break;
+		case KeyEvent.VK_A:
+			square.setLeftKeyPressed(false);
+			break;
+		case KeyEvent.VK_S:
+			square.setDownKeyPressed(false);
+			break;
 		}
 
-		
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		
+
 		rope.timePassed(circle, square);
 		circle.timePassed();
 		square.timePassed();
--- RopeOfHope/src/game/MainMenu.java
+++ RopeOfHope/src/game/MainMenu.java
@@ -14,17 +14,16 @@ import java.awt.event.ActionEvent;
 @SuppressWarnings("serial")
 public class MainMenu extends JFrame {
 	static final int LEVELS = 1;
-	static int level = 1; //change this when adding levels
+	static int level = 1; // change this when adding levels
 
 	static int highestLevelReached = 1;
-	static int circlex[] = {300,300};
-	static int circley[] = {200,200};
-	static int squarex[] = {200,200};
-	static int squarey[] = {200,200};
-	static int exitx[] = {150, 50};
-	static int exity[] = {200, 350};
+	static int circlex[] = { 300, 300 };
+	static int circley[] = { 200, 200 };
+	static int squarex[] = { 200, 200 };
+	static int squarey[] = { 200, 200 };
+	static int exitx[] = { 150, 50 };
+	static int exity[] = { 200, 350 };
 
-	
 	private JPanel contentPane;
 
 	/**
@@ -53,17 +52,17 @@ public class MainMenu extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
 		JButton btnStartGame = new JButton("Start Game");
 		contentPane.add(btnStartGame, BorderLayout.SOUTH);
 		btnStartGame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-		        setVisible(false);
+				setVisible(false);
 				startGame();
-				Audio.doAudioJunk("bg1");	
+				Audio.doAudioJunk("bg1");
 			}
 		});
-		
+
 		JButton btnLevelSelection = new JButton("Level Selection");
 		btnLevelSelection.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -71,12 +70,12 @@ public class MainMenu extends JFrame {
 			}
 		});
 		contentPane.add(btnLevelSelection, BorderLayout.WEST);
-		
+
 		JButton btnCredits = new JButton("Credits");
 		contentPane.add(btnCredits, BorderLayout.EAST);
 	}
-	
-	static void startGame(){
+
+	static void startGame() {
 		JFrame frame = new JFrame("Level " + (level));
 		frame.setBounds(100, 100, 900, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -84,42 +83,51 @@ public class MainMenu extends JFrame {
 		Level level = new Level();
 		frame.add(level);
 	}
-	
-	private void openLevelSelect(){
-	        this.setVisible(false);
-	        new LevelSelection().setVisible(true);
+
+	private void openLevelSelect() {
+		this.setVisible(false);
+		new LevelSelection().setVisible(true);
 	}
-	
-	public static void incrementLevel(){
+
+	public static void incrementLevel() {
 		level++;
-		if (highestLevelReached<level){
-			highestLevelReached=level;
+		if (highestLevelReached < level) {
+			highestLevelReached = level;
 		}
 	}
-	public static int getHighestLevelReached(){
+
+	public static int getHighestLevelReached() {
 		return highestLevelReached;
 	}
+
 	public static int getCirclex(int level) {
-		return circlex[level-1];
+		return circlex[level - 1];
 	}
+
 	public static int getCircley(int level) {
-		return circley[level-1];
+		return circley[level - 1];
 	}
+
 	public static int getSquarex(int level) {
-		return squarex[level-1];
+		return squarex[level - 1];
 	}
+
 	public static int getSquarey(int level) {
-		return squarey[level-1];
+		return squarey[level - 1];
 	}
+
 	public static int getExitx(int level) {
-		return exitx[level-1];
+		return exitx[level - 1];
 	}
+
 	public static int getExity(int level) {
-		return exity[level-1];
+		return exity[level - 1];
 	}
+
 	public static int getLevel() {
 		return level;
 	}
+
 	public static void setLevel(int l) {
 		level = l;
 	}
